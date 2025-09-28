b7759e3a2c0d1290cba2c26e32439333cd229dc9
Julian Vega <pamilo1@hotmail.com>
2025-09-28 15:14:02 -0500

    Changes

--- services/user_service.py
+++ services/user_service.py
@@ -1,2 +1,20 @@
-def register_user():
-    pass
+from project import User, Coach
+from database.repository import RepositoryProvider
+
+class UserService:
+
+    def register_user(self, id, name, age, clazz):
+        if self.user_repo.find(id):
+            raise ValueError("El usuario ya existe")
+        
+        if age <= 0:
+            raise ValueError("Edad inválida")
+
+        user = RepositoryProvider.get(clazz.__name__)
+        return user
+
+    def login_user(self, id):
+        user = self.user_repo.find(id)
+        if not user:
+            raise ValueError("Usuario no encontrado")
+        return user
