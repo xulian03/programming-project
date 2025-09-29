e15fe054d6c82cfebc50d9f4ff1234857cd592c2
Julian Vega <pamilo1@hotmail.com>
2025-09-28 20:52:14 -0500

    Update player_service.py

--- services/player_service.py
+++ services/player_service.py
@@ -17,13 +17,19 @@ class PlayerManagementService:
 
     def update_player_profile(self, player_id=None, **updates):
         players_repo = self.auth_service.players_repo
+        teams_repo = self.auth_service.teams_repo
         player: Player = players_repo.find(player_id) or self.auth_service.get_current_user()
         if not player:
             return False
+        player.set_name(updates.get("name"))
+        player.set_age(updates.get("age"))
         player.set_goals(updates.get("goals"))
         player.set_assists(updates.get("assists"))
         player.set_clearances(updates.get("clearences"))
-        player.set_age(updates.get("age"))
+        player.set_shots(updates.get("shots"))
+        player.set_shots_on_target(updates.get("shots_on_target"))
+        player.set_team(teams_repo.find(updates.get("team")))
+        player.set_position(updates.get("position"))
         
         return True
 
