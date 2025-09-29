b652e6c174d99dc4ab0c5e21517b7265ca0e4c5a
MozoTG349 <angel_mau.mozo@uao.edu.co>
2025-09-28 21:34:18 -0500

    Muchachos, ustedes escucharon ese rempalago?
    si.

--- project.py
+++ project.py
@@ -8,16 +8,47 @@ from database.repository import RepositoryProvider
 # Base Abstracta para Serialización
 # -------------------------------
 class Serializable(ABC):
+    """
+    Clase abstracta base para las entidades del sistema que deben almacenarse en JSON.
+    
+    Ofrece métodos de serialización y deserialización para convertir objetos en diccionarios 
+    y reconstruirlos después, lo que permite manejar de forma sencilla el guardado y la carga 
+    de datos.
+    """
 
     @abstractmethod
     def get_id(self):
+        """
+        Método abstracto que debe ser implementado por todas las clases hijas.
+        Retorna el identificador único de la entidad.
+        """
         pass
 
     def serialize(self):
+        """
+        Convierte el objeto en un diccionario listo para guardar.
+
+        Se basa en el atributo _serializable_attr, que cada clase define para indicar qué campos deben incluirse.
+
+        Returns:
+        dict: Diccionario con los atributos seleccionados del objeto.
+        """
         return {attr: getattr(self, attr) for attr in self._serializable_attr}
     
     @classmethod
     def deserialize(cls, data: dict):
+        """
+        Crea una instancia de la clase a partir de un diccionario.
+        
+        Remueve los guiones bajos de las claves para hacerlas compatibles
+        con los parámetros del constructor.
+        
+        Args:
+            data (dict): Diccionario con los datos del objeto
+            
+        Returns:
+            Instancia de la clase con los datos cargados
+        """
         clean_data = {k.lstrip("_"): v for k, v in data.items()}
         obj = cls(**clean_data)
         if not hasattr(obj, "_serializable_attr"):
